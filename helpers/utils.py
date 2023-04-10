cee5bfd48cfda948750b699719a458dfa1358b17
Amir Ziaee <ziaee.a.a@gmail.com>
2023-04-10 13:57:50 +0200

    added class_wise_accuracy

--- helpers/utils.py
+++ helpers/utils.py
@@ -24,3 +24,27 @@ def get_n_params(model):
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
     params = sum([np.prod(p.size()) for p in model_parameters])
     return params
+
+def class_wise_accuracy(confusion_matrix, target_names):
+    """
+    confusion matrix of multi-class classification
+    
+    target_names: name of a particular class 
+    
+    """
+    confusion_matrix = np.float64(confusion_matrix)
+    classwise_accuracy = {}
+    for i in range(confusion_matrix.shape[0]):
+        class_id = i
+        TP, FN, FP, TN, accuracy = 0, 0, 0, 0, 0
+        
+        TP = confusion_matrix[class_id,class_id]
+        FN = np.sum(confusion_matrix[class_id]) - TP
+        FP = np.sum(confusion_matrix[:,class_id]) - TP
+        TN = np.sum(confusion_matrix) - TP - FN - FP
+
+        #print(target_names[i], (TP,TN), (FP,FN))
+        accuracy = (TP+TN)/(TP+FP+FN+TN)
+        classwise_accuracy[target_names[i]] = accuracy
+        
+    return classwise_accuracy
